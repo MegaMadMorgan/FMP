b3bf806fa3e8e2a9bfb8c77ff194f5a07e2b7305
MegaMadMorgan <78374511+MegaMadMorgan@users.noreply.github.com>
2022-04-25 08:14:19 +0100

    + night music

--- Final Marked Project/Assets/AudioManager.cs
+++ Final Marked Project/Assets/AudioManager.cs
@@ -40,30 +40,66 @@ public class AudioManager : MonoBehaviour
         TrackRunTime -= Time.unscaledDeltaTime;
         if (time == "DAY")
         {
-            if (DayTrackList == 0 && TrackRunTime <= 0)
+            if ((DayTrackList == 0 || DayTrackList == 4) && TrackRunTime <= 0)
             {
                 play("DayMusic1");
                 TrackRunTime = 147;
                 DayTrackList += 1;
             }
-            if (DayTrackList == 1 && TrackRunTime <= 0)
+            if ((DayTrackList == 1 || DayTrackList == 5) && TrackRunTime <= 0)
             {
                 play("DayMusic2");
                 TrackRunTime = 178;
                 DayTrackList += 1;
             }
-            if (DayTrackList == 2 && TrackRunTime <= 0)
+            if ((DayTrackList == 2 || DayTrackList == 6) && TrackRunTime <= 0)
             {
                 play("DayMusic3");
                 TrackRunTime = 156;
                 DayTrackList += 1;
             }
-            if (DayTrackList == 3 && TrackRunTime <= 0)
+            if ((DayTrackList == 3 || DayTrackList == 7) && TrackRunTime <= 0)
             {
                 play("DayMusic4");
                 TrackRunTime = 169;
                 DayTrackList = 0;
             }
+        }
+        else
+        {
+            if ((DayTrackList == 4 || DayTrackList == 0) && TrackRunTime <= 0)
+            {
+                play("NightMusic1");
+                TrackRunTime = 147;
+                DayTrackList += 1;
+            }
+            if ((DayTrackList == 5 || DayTrackList == 1) && TrackRunTime <= 0)
+            {
+                play("NightMusic2");
+                TrackRunTime = 178;
+                DayTrackList += 1;
+            }
+            if ((DayTrackList == 6 || DayTrackList == 2) && TrackRunTime <= 0)
+            {
+                play("NightMusic3");
+                TrackRunTime = 156;
+                DayTrackList += 1;
+            }
+            if ((DayTrackList == 7 || DayTrackList == 3) && TrackRunTime <= 0)
+            {
+                play("NightMusic4");
+                TrackRunTime = 169;
+                DayTrackList = 4;
+            }
+        }
+
+        if (GameObject.Find("GameManager").GetComponent<LightingManager>().TimeOfDay > 6 && GameObject.Find("GameManager").GetComponent<LightingManager>().TimeOfDay < 18)
+        {
+            time = "NIGHT";
+        }
+        else
+        {
+            time = "DAY";
         }
 
         if (SceneManager.GetActiveScene().name != "City_Centre")
